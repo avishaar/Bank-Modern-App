f6b1369185e4d6dd687e8a3dcddf27843c7f3a4d
avishaar <ashaar928@gmail.com>
2023-05-18 13:18:42 +0300

    setup layout

--- Bank-Modern-App/src/App.jsx
+++ Bank-Modern-App/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '.style'
+import styles from './constants/style'
 
 import {  Navbar,
   Billing,
@@ -10,32 +10,32 @@ import {  Navbar,
   Stats,
   Footer,
   Testimonials,
-  Hero } from './components/Navbar';
+  Hero } from './components';
 
 const App = () => (
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Business
-          Billing
-          CardDeal
-          Testimonials
-          Clients
-          CTA
-          Footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
       </div>
 
