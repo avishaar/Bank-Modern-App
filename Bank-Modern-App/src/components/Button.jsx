5375633ab78ebfa953d653697915d67301c11f1b
avishaar <ashaar928@gmail.com>
2023-05-31 12:42:02 +0300

    billing section

--- Bank-Modern-App/src/components/Button.jsx
+++ Bank-Modern-App/src/components/Button.jsx
@@ -1,6 +1,4 @@
-import React from 'react'
-
-const Button = ({styles}) => {
+const Button = ({ styles }) => {
   return (
     <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
       Get Started
