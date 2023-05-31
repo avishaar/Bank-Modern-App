1dec81c7a2130764b1d9d8f6fee98f67098115b9
avishaar <ashaar928@gmail.com>
2023-05-31 12:51:20 +0300

    card deal section

--- Bank-Modern-App/src/components/CardDeal.jsx
+++ Bank-Modern-App/src/components/CardDeal.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import {card} from '../assets';
+import styles, {layout} from '../constants/style'
+import Button from './Button';
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden' /> in a few easy steps.</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>A good credit card deal offers favorable terms such as low interest rates, generous rewards programs, and minimal fees, providing consumers with the opportunity to maximize their financial benefits while effectively managing their expenses.</p>
+    
+      <Button styles='mt-10' />
+    
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt='card' className='w-[100%] h-[100%]' />
+    </div>
+
+  </section>
+)
 
 export default CardDeal;
